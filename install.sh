2d3048268719c07c9856655fbb296fc9dd1a0687
michal <michalsuski02@gmail.com>
2026-03-14 17:25:24 +0100

    Added Waybar & Wofi

--- install.sh
+++ install.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 sudo pacman -Syu --noconfirm
-sudo pacman -S --noconfirm git stow hyprland hyprpaper firefox kitty
+sudo pacman -S --noconfirm git stow hyprland hyprpaper firefox kitty waybar wofi
 
 cd ~/dotfiles
 stow hypr
 stow wallpaper
+stow waybar
+stow wofi
