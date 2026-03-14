28089cfa0d21e0019eed088c9b9d4779e5ffbcc0
michal <michalsuski02@gmail.com>
2026-03-14 22:08:17 +0100

    add fastfetch

--- bash/.bash_profile
+++ bash/.bash_profile
@@ -2,3 +2,5 @@ if [ -z "${DISPLAY}" ] && [ "$(tty)" = "/dev/tty1" ]; then
   exec start-hyprland
 fi
 export PS1="\[\033[0;92m\]\u\[\033[0m\]@arch:\[\033[0;94m\]\w\[\033[0m\]\$ "
+
+fastfetch
--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 sudo pacman -Syu --noconfirm
 sudo pacman -S --noconfirm git stow hyprland hyprpaper firefox kitty waybar wofi
-sudo pacman -S --noconfirm upower yazi
+sudo pacman -S --noconfirm upower yazi fastfetch
 
 sudo systemctl enable --now upower
 
