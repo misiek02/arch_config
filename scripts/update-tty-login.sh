5433dcbf7c94e8175d3ef69dddd66d45473628fc
michal <michalsuski02@gmail.com>
2026-03-21 18:03:37 +0100

    fix: double backslashes in TTY login logo for agetty compatibility

--- scripts/update-tty-login.sh
+++ scripts/update-tty-login.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 printf "\033c" > /etc/issue
-/usr/bin/fastfetch --config /home/michal/dotfiles/fastfetch/.config/fastfetch/tty.jsonc --pipe false >> /etc/issue
+
+/usr/bin/fastfetch --config /home/michal/dotfiles/fastfetch/.config/fastfetch/tty.jsonc --pipe false | sed 's/\\/\\\\/g' >> /etc/issue
+
 printf "\n" >> /etc/issue
