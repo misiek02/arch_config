5c108422c1fcbcf43822d15091771a9183c07537
michal <michalsuski02@gmail.com>
2026-03-21 14:34:13 +0100

    fix package name to network-manager-applet in install script

--- install.sh
+++ install.sh
@@ -2,9 +2,10 @@
 
 sudo pacman -Syu --noconfirm
 sudo pacman -S --noconfirm git stow hyprland hyprpaper firefox kitty waybar wofi
-sudo pacman -S --noconfirm upower yazi fastfetch code
+sudo pacman -S --noconfirm upower yazi fastfetch code networkmanager network-manager-applet
 
 sudo systemctl enable --now upower
+sudo systemctl enable --now NetworkManager
 
 cd ~/dotfiles
 stow bash
